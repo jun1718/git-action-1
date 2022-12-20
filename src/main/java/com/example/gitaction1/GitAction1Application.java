8092e6b47c404ef5f29cc82c86399a0db862e97c
jun1718 <gbeovhsqhtka@naver.com>
2022-12-20 16:45:31 +0900

    dk

---
+++ src/main/java/com/example/gitaction1/GitAction1Application.java
@@ -0,0 +1,14 @@
+package com.example.gitaction1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAction1Application {
+
+    public static void main(String[] args) {
+        System.out.println("dk");
+        SpringApplication.run(GitAction1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitaction1/GitAction1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitaction1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAction1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
